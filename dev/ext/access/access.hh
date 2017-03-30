0d8e6c705f3fae0da50da82d22f24df955d06b3f
chlewey <chlewey@gmail.com>
2017-03-29 22:48:43 -0500

    pass 1

---
+++ dev/ext/access/access.hh
@@ -0,0 +1,4 @@
+#ifndef __ACCESS_HH__
+#define __ACCESS_HH__
+#include "../../inc/h/interlecto.hh"
+#endif
---
+++ dev/ext/units/units.hh
@@ -0,0 +1,4 @@
+#ifndef __UNITS_HH__
+#define __UNITS_HH__
+#include "../../inc/h/interlecto.hh"
+#endif
